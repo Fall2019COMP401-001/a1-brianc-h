3e4c9f73134777c77719b12965b5c461bb7e9073
brianhouston <brianhouston@brians-mbp.wireless-1x.unc.edu>
2019-08-26 15:49:14 -0400

    Added first working version of A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,30 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		int numOfCustomers = scan.nextInt();
+		String[] output = new String[numOfCustomers];
 		
+		for (int i = 0; i < numOfCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numOfItems = scan.nextInt();
+			double totalCost = 0;
+			
+			for (int j = 0; j < numOfItems; j++) {
+				int quantity = scan.nextInt();
+				scan.next();
+				double price = scan.nextDouble();
+				totalCost += quantity * price;
+			}
+			
+			String costAsString = String.format("%.2f", totalCost);
+			output[i] = firstName.charAt(0) + ". " + lastName + ": " + costAsString;
+		}
+		
+		scan.close();
+		
+		for (int i = 0; i < numOfCustomers; i++) {
+			System.out.println(output[i]);
+		}
 	}
 }
