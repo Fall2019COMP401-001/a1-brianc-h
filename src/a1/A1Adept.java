2705ab09cb3516f7fbbcff46452707066f53b17d
brianhouston <brianhouston@brians-mbp.wireless-1x.unc.edu>
2019-08-26 16:39:55 -0400

    Added first working version of A1Adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,7 +8,66 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numOfItems = scan.nextInt();
+		String[] itemNames = new String[numOfItems];
+		double[] itemCosts = new double[numOfItems];
 		
+		for (int i = 0; i < numOfItems; i++) {
+			itemNames[i] = scan.next();
+			itemCosts[i] = scan.nextDouble();
+		}
+		
+		int numOfCustomers = scan.nextInt();
+		
+		String smallestSpender = "";
+		double smallestCost = Double.MAX_VALUE;
+		
+		String largestSpender = "";
+		double largestCost = 0.0;
+		
+		double cumulativeCost = 0.0;
+		
+		for (int i = 0; i < numOfCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numOfPurchases = scan.nextInt();
+			double totalCost = 0.0;
+			for (int j = 0; j < numOfPurchases; j++) {
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				
+				double cost = getCost(item, numOfItems, itemNames, itemCosts);
+				totalCost += cost * quantity;
+			}
+			
+			cumulativeCost += totalCost;
+			
+			if (totalCost > largestCost) {
+				largestSpender = firstName + " " + lastName;
+				largestCost = totalCost;
+			}
+			
+			if (totalCost < smallestCost) {
+				smallestSpender = firstName + " " + lastName;
+				smallestCost = totalCost;
+			}
+		}
+		
+		scan.close();
+		
+		System.out.println("Biggest: " + largestSpender + 
+				" (" + String.format("%.2f", largestCost) + ")");
+		System.out.println("Smallest: " + smallestSpender + 
+				" (" + String.format("%.2f", smallestCost) + ")");
+		System.out.println("Average: " + String.format("%.2f", cumulativeCost/numOfCustomers));
+	}
+	
+	public static double getCost(String id, int numOfItems, String[] items, double[] costs) {
+		for (int i = 0; i < numOfItems; i++) {
+			if (id.equals(items[i])) {
+				return costs[i];
+			}
+		}
+		return 0.0;
 	}
 }
