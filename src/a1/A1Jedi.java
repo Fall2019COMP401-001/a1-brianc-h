29081e0cd725aeec48dfd40e6403e91582078483
brianhouston <brianhouston@brians-mbp.wireless-1x.unc.edu>
2019-08-28 18:01:20 -0400

    Added first working version of A1Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,53 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numOfItems = scan.nextInt();
+		String[] itemNames = new String[numOfItems];
+		int[] customerPurchases = new int[numOfItems];
+		int[] itemsPurchased = new int[numOfItems];
 		
+		
+		for (int i = 0; i < numOfItems; i++) {
+			itemNames[i] = scan.next();
+			// ignore item cost
+			scan.nextDouble();
+		}
+		
+		int numOfCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numOfCustomers; i++) {
+			// ignore first and last names
+			scan.next();
+			scan.next();
+			int numOfPurchases = scan.nextInt();
+			for (int j = 0; j < numOfPurchases; j++) {
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				
+				int index = getIndexOfItem(item, numOfItems, itemNames);
+				customerPurchases[index]++;
+				itemsPurchased[index] += quantity;
+			}
+		}
+		
+		scan.close();
+		
+		for (int i = 0; i < numOfItems; i++) {
+			if (customerPurchases[i] == 0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			} else {
+				System.out.println(customerPurchases[i] + " customers bought "
+						+ itemsPurchased[i] + " " + itemNames[i]);
+			}	
+		}
+	}
+	
+	public static int getIndexOfItem(String id, int numOfItems, String[] items) {
+		for (int i = 0; i < numOfItems; i++) {
+			if (id.equals(items[i])) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
